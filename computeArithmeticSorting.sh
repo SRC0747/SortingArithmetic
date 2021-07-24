fa6cfc5a5e129803f76bba815c6f62277eabc5f7
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 01:43:19 +0530

    perform the arithmetic operation of (a+b*c)

--- computeArithmeticSorting.sh
+++ computeArithmeticSorting.sh
@@ -3,3 +3,4 @@ echo "Welcome to Arithmetic computation and Sorting problem."
 read -p "Enter the value of a:" a
 read -p "Enter the value of b:" b
 read -p "Enter the value of c:" c
+m=$(($a+($b*$c)))
