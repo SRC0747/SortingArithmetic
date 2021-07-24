e2e11d2a684f277a6a483b25fb11ce9576dfd6d2
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 01:39:14 +0530

    Entering three inputs to do arithmetic computation.

--- computeArithmeticSorting.sh
+++ computeArithmeticSorting.sh
@@ -1,2 +1,5 @@
 #!/bin/bash -x
 echo "Welcome to Arithmetic computation and Sorting problem."
+read -p "Enter the value of a:" a
+read -p "Enter the value of b:" b
+read -p "Enter the value of c:" c
