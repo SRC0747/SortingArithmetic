7351cfc6353b9e41117381496c9e488626d28f9e
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 20:01:44 +0530

    Reading the values from a dictionary into an array.

--- computeArithmeticSorting.sh
+++ computeArithmeticSorting.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 echo "Welcome to Arithmetic computation and Sorting problem."
 declare -A arithmetic
+declare -a result
 read -p "Enter the value of a:" a
 read -p "Enter the value of b:" b
 read -p "Enter the value of c:" c
@@ -13,3 +14,5 @@ arithmetic[compute2]="$n"
 arithmetic[compute3]="$p"
 arithmetic[compute4]="$q"
 echo "${arithmetic[@]}"
+result=${arithmetic[@]}
+echo "${result[@]}"
