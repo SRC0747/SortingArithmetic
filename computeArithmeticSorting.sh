58ba3366ee862b7225cabc6c55fd0a4c5c0d4ac9
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 19:56:12 +0530

    Storing the result of the every arithmetic computation in a dictionary.

--- computeArithmeticSorting.sh
+++ computeArithmeticSorting.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 echo "Welcome to Arithmetic computation and Sorting problem."
+declare -A arithmetic
 read -p "Enter the value of a:" a
 read -p "Enter the value of b:" b
 read -p "Enter the value of c:" c
@@ -7,3 +8,8 @@ m=$(($a+($b*$c)))
 n=$((($a*$b)+$c))
 p=$(($c+($a/$b)))
 q=$((($a%$b)+$c))
+arithmetic[compute1]="$m"
+arithmetic[compute2]="$n"
+arithmetic[compute3]="$p"
+arithmetic[compute4]="$q"
+echo "${arithmetic[@]}"
